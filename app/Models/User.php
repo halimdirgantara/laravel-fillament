5587f9e8f3caf2b6fe75bd1f23081933dc5d268b
halimdirgantara <halimdirgantara@gmail.com>
2023-06-20 20:24:56 +0700

    feat: spatie permissin and seeder user model edit

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,20 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
+    use SoftDeletes;
+    use HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -19,8 +24,15 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'nip',
+        'nik',
+        'phone',
+        'address',
         'email',
         'password',
+        'organization_id',
+        'is_online',
+        'is_active',
     ];
 
     /**
@@ -31,6 +43,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
     ];
 
     /**
@@ -40,6 +54,55 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array<int, string>
+     */
+    protected $appends = [
+        'profile_photo_url',
+    ];
+
+    public function organization()
+    {
+        return $this->belongsTo(Organization::class, 'organization_id');
+    }
+    public function userincreated(): HasMany
+    {
+        return $this->hasMany(Post::class, 'created_by', 'id');
+    }
+    public function postShared(): HasMany
+    {
+        return $this->hasMany(Post::class, 'created_by', 'id');
+    }
+    public function userinupdated(): HasMany
+    {
+        return $this->hasMany(Post::class, 'updated_by', 'id');
+    }
+    public function userFile(): HasMany
+    {
+        return $this->hasMany(File::class, 'created_by', 'id');
+    }
+    public function userGallery(): HasMany
+    {
+        return $this->hasMany(Gallery::class, 'created_by', 'id');
+    }
+    public function userContactUs(): HasMany
+    {
+        return $this->hasMany(ContactUs::class, 'read_by', 'id');
+    }
+    public function userTag(): HasMany
+    {
+        return $this->hasMany(ContactUs::class, 'created_by', 'id');
+    }
+    public function userCategory(): HasMany
+    {
+        return $this->hasMany(Category::class, 'created_by', 'id');
+    }
+    public function userSocialMedia(): HasMany
+    {
+        return $this->hasMany(SocialMedia::class, 'created_by', 'id');
+    }
 }
